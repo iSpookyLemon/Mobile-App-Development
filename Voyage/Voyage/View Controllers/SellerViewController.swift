9c1fca718fe7fe9e2ed22cff14519d6d42f6e3cf
skumar2006 <shivamkumarr06@gmail.com>
2021-05-26 11:34:24 -0500

    New viewcontrollers

---
+++ Voyage/Voyage/View Controllers/SellerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SellerViewController.swift
+//  Voyage
+//
+//  Created by Shivam Kumar on 5/26/21.
+//
+
+import UIKit
+
+class SellerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
