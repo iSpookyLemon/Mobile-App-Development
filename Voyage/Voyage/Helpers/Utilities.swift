a739affca7964f12da3756e0c0dd360ac6c39446
skumar2006 <shivamkumarr06@gmail.com>
2021-05-27 01:23:51 -0500

    Improved the seller page

--- Voyage/Voyage/Helpers/Utilities.swift
+++ Voyage/Voyage/Helpers/Utilities.swift
@@ -28,10 +28,35 @@ class Utilities {
         
     }
     
+    static func invertedStyleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.init(red: 255/255, green: 135/255, blue: 6/255, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+    }
+    
     static func styleFilledButton(_ button:UIButton) {
         
         // Filled rounded corner style
         button.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func invertedStyleFilledButton(_ button:UIButton) {
+        
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 255/255, green: 135/255, blue: 6/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
--- Voyage/Voyage/View Controllers/SellerViewController.swift
+++ Voyage/Voyage/View Controllers/SellerViewController.swift
@@ -6,16 +6,56 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class SellerViewController: UIViewController {
+class SellerViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
+    @IBOutlet weak var freelanceServiceTextField: UITextField!
+    
+    @IBOutlet weak var dollarsPerHourTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var importImageImageView: UIImageView!
+    
+    @IBAction func importImageButton(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated:true){
+            //After it is complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            importImageImageView.image = image
+        }
+        else
+        {
+            //error message
+        }
+        
+        self.dismiss(animated:true, completion:nil)
+    }
+    
+    @IBOutlet weak var signUpServiceButton: UIButton!
+    
+    func setUpElements() {
+        Utilities.invertedStyleTextField(freelanceServiceTextField)
+        Utilities.invertedStyleTextField(dollarsPerHourTextField)
+        Utilities.invertedStyleTextField(phoneNumberTextField)
+        Utilities.invertedStyleFilledButton(signUpServiceButton)
+    }
     /*
     // MARK: - Navigation
 
