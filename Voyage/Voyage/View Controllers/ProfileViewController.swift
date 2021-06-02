737e2fe17476c0ca191990e9fdca9dedefe167d4
iSpookyLemon <77311956+iSpookyLemon@users.noreply.github.com>
2021-06-01 22:04:25 -0700

    Profile

--- Voyage/Voyage/View Controllers/ProfileViewController.swift
+++ Voyage/Voyage/View Controllers/ProfileViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var name: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let uid = Auth.auth().currentUser!.uid
+        
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("users").document(uid)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let firstName = document.get("firstname") as? String ?? ""
+                let lastName = document.get("lastname") as? String ?? ""
+                
+                self.name.text = firstName + " " + lastName
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document does not exist")
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
